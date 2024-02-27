5dc3d5c1b25b43df4d0bce12f06e6a4d2f63eb74
jiylee <jjenny4711@gmail.com>
2024-02-26 23:44:24 -0500

    add package json

--- controller/area.controller.js
+++ controller/area.controller.js
@@ -1,5 +1,4 @@
 const {getFromAI} =require('../utils/ai')
-const Area=require('../model/area')
 const areaController={}
 
 areaController.makeResult=async(req,res)=>{
@@ -7,9 +6,8 @@ areaController.makeResult=async(req,res)=>{
     const {date,city,country,weather,kind}=req.body;
     console.log(req.body,'mainsssssssss')
     let result = await getFromAI(date,city,country,weather,kind)
-    console.log(result,'mmmmmmmmmmmm')
-    // if(!result)throw new Error('error for makeResult')
-    console.log(result,'result!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    
+console.log(result,'result')
     res.status(200).json({data:result})
   }catch(error){
 console.log(error,'error!!!!!!!!!!!!')
--- utils/ai.js
+++ utils/ai.js
@@ -1,5 +1,5 @@
 const OpenAI = require('openai');
-const OPEN_AI_KEY1 = process.env.OPEN_AI_KEY1;
+
 const fs = require('fs')
 require("dotenv").config()
 console.log(process.env.OPEN_AI_KEY1,'!!!!!!!!!@@@@')
@@ -20,21 +20,55 @@ const getFromAI = async (date,country,city,weather,kind) => {
         관심사: ${kind}
         
         날씨: ${weather}
+        지역:${city} ,${country}
         
-        요청: country: ${country},city:${city}여행 관련 관광지 정보를 JSON 파일 형식으로 받을 수 있도록 해주세요. 각 관광지에는 장소, 주소, 설명, 이미지 URL이 포함되어야 합니다.`},
+        요청: 알려드린 정보를 바탕으로 여행 관련 관광지 정보를 location, address, description, latitude ,longitude 가포함되어야 합니다.`},
       ],
+      functions: [{
+        name: "tour",
+        description: "get infomation of location.한국말로 해주세요.",
+        parameters: {
+          type: "object",
+          properties: {
+            location: {
+              type: "string",
+              description: "The location,e.g.경복궁."
+            },
+            address: {
+              type: "string",
+              description: `address of the location,e.g.대한민국 서울특별시 종로구 사직로 161.please speak in ${country}`
+            },
+            latitude: {
+              type: "string",
+              description: "The latitude of the Location,e.g.37.579617"
+            },
+            longigude: {
+              type: "string",
+              description: "The longigude of the Location,e.g.126.977041"
+            },
+            description: {
+              type: "string",
+              description: "The explain of the Location"
+            }
+          },
+          required: ["location", "address", "latitude", "longigude", " description"]
+        }
+      }],
+      function_call:"auto"
     });
   
-    const data = {
-      date: date,
-      destination: city,
-      interest: kind,
-      suggested_spots: response.choices[0].message.content.split('\n'), // 개행을 기준으로 분리하여 배열로 변환
-    };
-    fs.writeFileSync('suggested_spots.json', JSON.stringify(data, null, 2));
-    console.log('JSON file saved successfully.');
-console.log('result',response.choices[0].message.content)
-    return response.choices[0].message.content
+console.log('result',response.choices[0].message.content,'data')
+const res = response.choices[0].message
+let completionArguments;
+if(!res.content ){
+  const functionCallName = res.function_call.name;
+  console.log(functionCallName,'name!!!!!!!!!')
+  if(functionCallName === "tour"){
+  completionArguments =JSON.parse(res.function_call.arguments)
+    console.log(completionArguments,'arg!!!!!!!!!!!!!!!!!!!!!!!!!')
+  }
+}
+    return completionArguments
 
 
 
