88bb3c326e3692961dcde90eb1c487a552f4d90a
jiylee <jjenny4711@gmail.com>
2024-02-28 01:11:25 -0500

    all

--- controller/area.controller.js
+++ controller/area.controller.js
@@ -8,13 +8,13 @@ areaController.makeResult=async(req,res)=>{
     console.log(req.body,'mainsssssssss')
     // let result = await getFromAI(date,city,country,weather,kind)
     let result =await getFromAIAndSaveAsJSON(date,city,country,weather,kind);
-    const tipsArray = result.split('\n').map(tip => tip.trim()).filter(tip => tip.length > 0);
-    const resultObj=createObj(tipsArray)
-console.log(resultObj,'result!!!!!!!!!!!dddd')
+//     const tipsArray = result.split('\n').map(tip => tip.trim()).filter(tip => tip.length > 0);
+//     const resultObj=createObj(tipsArray)
+// console.log(resultObj,'result!!!!!!!!!!!dddd')
     
 
 
-    res.status(200).json({data:resultObj})
+    res.status(200).json({data:result})
   }catch(error){
 console.log(error,'error!!!!!!!!!!!!')
 }
