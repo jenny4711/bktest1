6322519dc3ebc066529e0f17f21fb15834a7e8d9
jiylee <jjenny4711@gmail.com>
2024-02-28 03:03:50 -0500

    all

--- utils/ai.js
+++ utils/ai.js
@@ -18,7 +18,7 @@ const getFromAIAndSaveAsJSON = async (date, country, city, weather, kind) => {
       model: 'gpt-3.5-turbo',
       messages: [
         { role: 'system', content: `You are a travel guide for ${city}, ${country}.` },
-        { role: 'user', content: `Date: ${date}\nInterests: ${kind}\nWeather: ${weather}\nLocation: ${city}, ${country}\nRequest: Based on the above information, please provide 4 travel destinations with name, address, description, latitude, and longitude included. Please provide the information in Korean.DO NOT SHOW 1.,2.,3.,4.,5 in front of name please.` },
+        { role: 'user', content: `Date: ${date}\nInterests: ${kind}\nWeather: ${weather}\nLocation: ${city}, ${country}\nRequest: Based on the above information, please provide 4 travel destinations with name of travel destination, address, description, latitude, and longitude included. Please provide the information in Korean.DO NOT SHOW 1.,2.,3.,4.,5 in front of name please.` },
       ]
     });
 
@@ -46,7 +46,7 @@ const getRestaurantFromAI = async (date, country, city, weather, kind) => {
       model: 'gpt-3.5-turbo',
       messages: [
         { role: 'system', content: `You are a guide who knows the best restaurants in ${city}, ${country}.` },
-        { role: 'user', content: `Date: ${date}\nInterests: ${kind}\nWeather: ${weather}\nLocation: ${city}, ${country}\nRequest:차례 번호 없이, 레스토랑의 이름, 위치, 주소, 설명, 위도 및 경도를 포함하여 좋은 레스토랑 5곳의 정보를 한국어로 알려주세요.1.,2.,3.,4.,5 제발 넣지 말아주세요.` },
+        { role: 'user', content: `Date: ${date}\nInterests: ${kind}\nWeather: ${weather}\nLocation: ${city}, ${country}\nRequest:차례 번호 없이, 레스토랑의 이름, 위치, 주소, 설명, 위도, 경도 를 포함하여 좋은 레스토랑 5곳의 정보를 한국어로 알려주세요.1.,2.,3.,4.,5 제발 빼고 주세요.` },
       ]
     });
 
