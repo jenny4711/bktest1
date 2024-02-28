f656ffa679a363785dc3a163c9ce22ac0a535432
jiylee <jjenny4711@gmail.com>
2024-02-28 03:30:53 -0500

    eee

--- utils/ai.js
+++ utils/ai.js
@@ -12,19 +12,13 @@ const openai = new OpenAI({
 
 const getFromAIAndSaveAsJSON = async (date, country, city, weather, kind) => {
   try {
-    const form={
-      name:"여행명소 의 이름",
-      address:"여행명소 의 주소",
-      description:"여행명소 의 설명",
-      latitude:"위도",
-      longitude:"경도"
-     }
+
 
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
       messages: [
         { role: 'system', content: `You are a travel guide for ${city}, ${country}.` },
-        { role: 'user', content: `Date: ${date}\nInterests: ${kind}\nWeather: ${weather}\nLocation: ${city}, ${country}\nRequest:알려드린 정보를 바탕으로 , 추천 여행 명소 5곳의 정보를 ${form} 같이 한국어로 알려주세요.` },
+        { role: 'user', content: `Date: ${date}\nInterests: ${kind}\nWeather: ${weather}\nLocation: ${city}, ${country}\nRequest: Based on the above information, please provide 4 travel destinations with name of travel destination, address, description, latitude, and longitude included. Please provide the information in Korean.` },
       ]
     });
 
@@ -46,28 +40,20 @@ const getFromAIAndSaveAsJSON = async (date, country, city, weather, kind) => {
 
 const getRestaurantFromAI = async (date, country, city, weather, kind) => {
   try {
-   const form={
-    name:"레스토랑 의 이름",
-    address:"레스토랑 의 주소",
-    description:"레스토랑 의 설명",
-    latitude:"위도",
-    longitude:"경도"
-   }
+    // 요청할 정보 형식을 텍스트로 명시적으로 기술
+
 
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
       messages: [
         { role: 'system', content: `You are a guide who knows the best restaurants in ${city}, ${country}.` },
-        { role: 'user', content: `Date: ${date}\nInterests: ${kind}\nWeather: ${weather}\nLocation: ${city}, ${country}\nRequest:차례 번호 없이, ${form} 같이 좋은 레스토랑 5곳의 정보를 한국어로 알려주세요.` },
+        { role: 'user', content: `Date: ${date}\nInterests: ${kind}\nWeather: ${weather}\nLocation: ${city}, ${country}\nRequest: Request: Without order numbers, please provide information on 5 good restaurants including the restaurant's name, location, address, description, latitude, and longitude in Korean.` },
       ]
     });
 
-   
-
-    return response.choices[0].message.content
+    return response.choices[0].message.content;
   } catch (error) {
     console.error('Error creating chat completion:', error);
-   
   }
 }
 
